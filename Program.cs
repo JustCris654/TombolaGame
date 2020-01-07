68c6f8aceda1c7ace9919579a391375c35b4ed9e
justcris <justcris654@gmail.com>
2020-01-07 10:50:44 +0100

    Project initialized

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TombolaGame_Project {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
